db048ecaa0c736271eab73eadb49a51a304021bd
AleksHodur <aleksandra.hodur.jaromin@gmail.com>
2024-02-27 14:07:38 +0100

    a backend was born

---
+++ backend/src/app.ts
@@ -0,0 +1,18 @@
+import express, { Express, Request, Response, NextFunction } from "express";
+
+const app: Express = express();
+
+const port: number = 4000;
+
+app.listen(port, () => {
+    console.log('Server is listening on port ' + port);
+});
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    console.log('Request received: ', req.url);
+    next();
+});
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("Hi! This is server listening :)");
+});
